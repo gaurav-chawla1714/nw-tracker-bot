9829692b0dd74deac62b27bdfd9198b128aa546e
gaurav-chawla1714 <gaurav.chawla1714@gmail.com>
2024-05-02 21:51:00 -0400

    adding sheets capabilities

--- bot.py
+++ bot.py
@@ -6,14 +6,17 @@ from PIL import Image
 import re
 import pytesseract
 import time
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
 
 load_dotenv()
 
+API_SCOPES = os.getenv("API_SCOPES").split()
+SERVICE_ACCOUNT_PATH = os.getenv("SERVICE_ACCOUNT_PATH")
 pytesseract.pytesseract.tesseract_cmd = os.getenv('TESSERACT_PATH')
-
 BOT_TOKEN = os.getenv('BOT_TOKEN')
-
 CHANNEL_ID = int(os.getenv('CHANNEL_ID'))
+SPREADSHEET_ID = os.getenv('SPREADSHEET_ID')
 
 custom_intents = discord.Intents.none()
 custom_intents.message_content = True
@@ -21,6 +24,7 @@ custom_intents.guilds = True
 custom_intents.guild_messages = True
 
 bot = commands.Bot(command_prefix="!", intents=custom_intents, help_command=None)
+
 
 @bot.event
 async def on_ready():
@@ -47,10 +51,20 @@ async def status(ctx, *args):
     else:
         await ctx.send("You didn't specify what status you wanted.")
 
+@bot.command()
+async def r(ctx):
+    sheets_service = create_sheets_service()
+
+    values = read_sheet(sheets_service, "A1:AA100")
+
+    await ctx.send(values)
+
 
 @bot.command()
 async def p(ctx):
-    if ctx.message.attachments:
+    if not ctx.message.attachments:
+        await ctx.send("Please include an image for me to parse.")
+    else:
         attachment = ctx.message.attachments[0]
         message_timestamp = ctx.message.created_at
 
@@ -71,14 +85,34 @@ async def p(ctx):
         await ctx.send(f'Assets: ${str(assets)}')
         await ctx.send(f'Liabilities: ${str(liabilities)}')
         await ctx.send(f'Net Worth: ${str(round(assets + liabilities, 2))}')
-    else:
-        await ctx.send("Please include an image for me to parse.")
+
+
+
 
 
 def get_local_time():
     return time.strftime("%H:%M:%S", time.localtime()) + " (EST)."
 
 def convert_money_to_float(money_str: str) -> float:
-    return float(money_str.replace('$', '').replace(',', '')) 
+    return float(money_str.replace('$', '').replace(',', ''))
+
+def create_sheets_service():
+    credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_PATH, scopes=API_SCOPES)
+
+    service = build('sheets', 'v4', credentials=credentials)
+
+    return service
+
+def read_sheet(service, range_name):
+    sheet = service.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID, range=range_name).execute()
+    values = sheet.get('values', [])
+    if not values:
+        print('No data found.')
+    else:
+        for row in values:
+            print(row)
+
+    return values
+
 
 bot.run(BOT_TOKEN)
